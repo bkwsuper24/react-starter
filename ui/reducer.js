788dd4fa56ffef470969676ca7d74e04bf9d57d1
Brian Wang <bkwsuper24@gmail.com>
2016-09-08 15:30:26 -0700

    Basic Reducer

---
+++ ui/reducer.js
@@ -0,0 +1,10 @@
+export default function images(state, action) {
+	switch(action.type) {
+	case "TESTING":
+		console.log("Entered");
+	case "T2":
+		console.log("Entered");
+	default:
+	return state;
+	}
+}
